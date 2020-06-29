0b8a8d1bcd65aa75465c646a18d3c585dfc9105c
juandsuarezz <1486@holbertonschool.com>
2020-06-29 17:50:27 +0000

    [Fix]: JSON datetime

--- models/base_model.py
+++ models/base_model.py
@@ -14,20 +14,20 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
 
+
     def __str__(self):
 
         return ("[{}] ({}) {}".format(str(type(self).__name__),
-                                        self.id, str(self.__dict__)))
+                                      self.id, str(self.__dict__)))
 
     def save(self):
 
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,24 +4,30 @@ import json
 import models
 
 class FileStorage:
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+
         return (self.__objects)
 
     def new(self, obj):
+
         if obj:
             self.__objects["{}.{}".format(str(type(obj).__name__),
                                           obj.id)] = obj
+
     def save(self):
-        dic = {}
-        for id, objs in self.__objects.items():
-            dic[id] = objs.to_dict()
+
+        for key, value in self.__objects.items():
+            if not isinstance(value, dict):
+                self.__objects[key] = value.to_dict()
         with open(self.__file_path, mode="w", encoding="UTF-8") as f:
-            json.dump(dic, f)
+            json.dump(self.__objects, f)
 
     def reload(self):
+
         try:
             with open(self.__file_path, encoding="UTF-8") as f:
                 obj = json.load(f)
